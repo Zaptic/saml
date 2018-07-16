4e2f4367052be13bdc4f5af8480d742eb8ce79dc
Ben <ben@zaptic.com>
2018-07-16 15:27:52 +0100

    Remove unused methods

--- src/xml.ts
+++ src/xml.ts
@@ -1,34 +1,5 @@
-import { DOMParser } from 'xmldom'
-import * as xpath from 'xpath'
 import * as path from 'path'
 import * as xsd from 'libxml-xsd'
-
-export type XPath = string
-
-type Mapping = { [key: string]: XPath }
-
-export function extractFields(xmlString: string, mapping: Mapping) {
-    const document = new DOMParser().parseFromString(xmlString)
-    const result: { [key in keyof Mapping]: string } = {}
-
-    for (const key in mapping) {
-        if (!mapping.hasOwnProperty(key)) continue
-        const element = xpath.select(mapping[key], document).map(attribute => attribute.nodeValue.toString())
-
-        if (element.length !== 1) throw new Error(`Expected only one element for ${mapping[key]}`)
-        result[key] = element[0]
-    }
-
-    return result
-}
-
-export function getAttribute(name: string, attr: string): XPath {
-    return `//*[local-name(.)='${name}']/@${attr}`
-}
-
-export function getText(name: string): XPath {
-    return `//*[local-name(.)='${name}']/text()`
-}
 
 export function loadXSD(toLoad: string) {
     return new Promise<{ validate: xsd.ValidateFunction }>((resolve, reject) => {
