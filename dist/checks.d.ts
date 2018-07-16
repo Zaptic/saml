32811f8c8625e862964065acf345d8ca825190c5
Ben <ben@zaptic.com>
2018-07-16 11:28:06 +0100

    Stop tracking dist
    It's not such a good idea after all. Now that the scripts are there to
    ensure that releases have the latest dist we should not have the initial
    problem that cause us to check this in. This is also motivated by the
    fact that having dist checked in makes it hard to follow actual code
    changes using gitk or git diff.

--- dist/checks.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function checkTime(notBefore?: Date, notOnOrAfter?: Date, strict?: boolean): boolean;
-export declare function checkStatusCode(status: string): boolean;
--- dist/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function normalizeCertificate(certificate: string): string;
-export declare function toBase64(message: string): string;
-export declare function fromBase64(message: string): string;
