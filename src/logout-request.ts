38bb7130b2b76975c988f653664cd91de291e007
Ben <ben@zaptic.com>
2018-07-17 08:52:54 +0100

    Add logout request parser
    In order to be able to extract information from the logout requests our
    service provider might receive from an identity provider

---
+++ src/logout-request.ts
@@ -0,0 +1,44 @@
+import { Signature } from './helpers/saml-types'
+import { parseXML } from './helpers/xml'
+
+export namespace SAMLLogoutRequest {
+    export type Root = {
+        $: {
+            ID: string
+            Version: string
+            IssueInstant: string
+            Destination: string
+        }
+        Issuer: string[]
+        NameID: {
+            _: string
+            $: {
+                SPNameQualifier: string
+                Format: string
+            }
+        }[]
+        Signature: Signature[]
+    }
+}
+
+type LogoutRequest = {
+    id: string
+    issueInstant: string
+    destination: string
+    issuer: string
+    names: string[]
+}
+
+export async function extract(response: string): Promise<LogoutRequest> {
+    const jsonResponse = await parseXML<SAMLLogoutRequest.Root>(response)
+
+    const parsedResponse = {
+        id: jsonResponse.$.ID,
+        issueInstant: jsonResponse.$.IssueInstant,
+        destination: jsonResponse.$.Destination,
+        issuer: jsonResponse.Issuer[0],
+        names: jsonResponse.NameID.map(NameId => NameId._)
+    }
+
+    return parsedResponse
+}
